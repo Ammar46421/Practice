4e845c935e94b9723f3402ef4d41d7d95687e3a3
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-05-01 18:17:06 +0500

    practicce

---
+++ style.js
@@ -0,0 +1,22 @@
+var city1 = "karachi";
+var city2 = "lahore";
+var city3 = "islamabad";
+var city4 = "hyderabad";
+var city5 = "sukkur";
+
+var city = prompt(city);
+if(city === city1){
+    alert("Mobile Nikal Karwe")
+};
+if(city === city2){
+    alert("Lahore Lahore ee")
+};
+if(city === city3){
+    alert("hi bro whassup")
+};
+if(city === city4){
+    alert("ada chanh piyanden?")
+};
+if(city === city5){
+    alert("dawat athai location kachaa")
+};
